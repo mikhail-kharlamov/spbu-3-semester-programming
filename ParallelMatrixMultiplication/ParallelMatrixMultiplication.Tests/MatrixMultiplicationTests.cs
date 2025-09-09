aace65ca0c28dee8e86a91edca7dd762e88c35cd
michaelharlam <palptin2014@gmail.com>
2025-09-10 01:07:54 +0300

    feat: added classes for matrix object with file interaction and sequential and parallel matrix multiplication. added tests

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication.Tests/MatrixMultiplicationTests.cs
@@ -0,0 +1,60 @@
+// <copyright file="MatrixMultiplicationTests.cs" company="Mikhail Kharlamov">
+// Copyright (c) Mikhail Kharlamov. All rights reserved.
+// </copyright>
+
+namespace ParallelMatrixMultiplication;
+
+/// <summary>
+/// Class with tests for matrix multiplication.
+/// </summary>
+public class MatrixMultiplicationTests
+{
+    /// <summary>
+    /// Test that checks that Multiply method works.
+    /// </summary>
+    [Test]
+    public void Multiply_Simple2x2_Works()
+    {
+        var left = Matrix.FromArrays([[1, 2], [3, 4]]);
+        var right = Matrix.FromArrays([[2, 0], [1, 2]]);
+
+        var result = MatrixMultiplication.Multiply(left, right);
+        Assert.That(result.GetRow(0), Is.EquivalentTo(new[] { 4, 4 }));
+        Assert.That(result.GetRow(1), Is.EquivalentTo(new[] { 10, 8 }));
+    }
+
+    /// <summary>
+    /// Test that checks that MultiplyParallel method works.
+    /// </summary>
+    [Test]
+    public void MultiplyParallel_Simple2x2_Works()
+    {
+        var left = Matrix.FromArrays([[1, 2], [3, 4]]);
+        var right = Matrix.FromArrays([[2, 0], [1, 2]]);
+
+        var result = MatrixMultiplication.MultiplyParallel(left, right);
+        Assert.That(result.GetRow(0), Is.EquivalentTo(new[] { 4, 4 }));
+        Assert.That(result.GetRow(1), Is.EquivalentTo(new[] { 10, 8 }));
+    }
+
+    /// <summary>
+    /// Test that checks similarity of sequential and parallel computation of matrix multiplication.
+    /// </summary>
+    [Test]
+    public void MultiplyParallel_GivesSameResultAsSequential()
+    {
+        var left = Matrix.FromArrays([[1, 2, 3], [4, 5, 6]]);
+        var right = Matrix.FromArrays([[7, 8], [9, 10], [11, 12]]);
+
+        var sequential = MatrixMultiplication.Multiply(left, right);
+        var parallel = MatrixMultiplication.MultiplyParallel(left, right);
+
+        for (var i = 0; i < sequential.Rows; i++)
+        {
+            for (var j = 0; j < sequential.Columns; j++)
+            {
+                Assert.That(parallel.GetCell(i, j), Is.EqualTo(sequential.GetCell(i, j)));
+            }
+        }
+    }
+}
---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication.Tests/MatrixTests.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="MatrixTests.cs" company="Mikhail Kharlamov">
+// Copyright (c) Mikhail Kharlamov. All rights reserved.
+// </copyright>
+
+namespace ParallelMatrixMultiplication;
+
+/// <summary>
+/// Class with tests for class Matrix.
+/// </summary>
+public class MatrixTests
+{
+    /// <summary>
+    /// Tests for creation by fabric method .FromArrays with valid data.
+    /// </summary>
+    [Test]
+    public void FromArrays_ValidData_CreatesMatrix()
+    {
+        var data = new[]
+        {
+            new[] { 1, 2, 3 },
+            new[] { 4, 5, 6 },
+        };
+
+        var matrix = Matrix.FromArrays(data);
+
+        Assert.That(matrix.Rows, Is.EqualTo(2));
+        Assert.That(matrix.Columns, Is.EqualTo(3));
+        Assert.That(matrix.GetCell(1, 1), Is.EqualTo(5));
+    }
+
+    /// <summary>
+    /// Tests for creation by fabric method .FromArrays with invalid data.
+    /// </summary>
+    [Test]
+    public void FromArrays_InvalidData_Throws()
+    {
+        var data = new[]
+        {
+            new[] { 1, 2 },
+            new[] { 3 },
+        };
+
+        Assert.Throws<ArgumentException>(() => Matrix.FromArrays(data));
+    }
+
+    /// <summary>
+    /// Tests for dumping to file by method .ToFile.
+    /// </summary>
+    [Test]
+    public void ToFile_And_FromFile_RoundTrip_Works()
+    {
+        var data = new[]
+        {
+            new[] { 1, 2, 3 },
+            new[] { 4, 5, 6 },
+        };
+
+        var matrix = Matrix.FromArrays(data);
+
+        var tempFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".txt");
+        matrix.ToFile(tempFile);
+
+        var loaded = Matrix.FromFile(tempFile);
+        Assert.That(matrix.Rows, Is.EqualTo(loaded.Rows));
+        Assert.That(matrix.Columns, Is.EqualTo(loaded.Columns));
+        Assert.That(matrix.GetCell(1, 1), Is.EqualTo(loaded.GetCell(1, 1)));
+    }
+}
