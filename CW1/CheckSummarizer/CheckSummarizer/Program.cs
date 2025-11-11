d06b297b3f1d7aa8b1c303d5c83b6984bd67efd5
michaelharlam <palptin2014@gmail.com>
2025-11-11 15:23:44 +0300

    feat:: added console app

--- CW1/CheckSummarizer/CheckSummarizer/Program.cs
+++ CW1/CheckSummarizer/CheckSummarizer/Program.cs
@@ -2,14 +2,51 @@
 // Copyright (c) Mikhail Kharlamov. All rights reserved.
 // </copyright>
 
+using System.Runtime.CompilerServices;
+
 namespace CheckSummarizer;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine(
-            "Hi, this is a utility for calculating md5 hashes across files and folders. usage:\n<path> - single-threaded implementation\n<path> -p - multithreaded implementation");
+        if (args.Length == 0 || args.Length > 2)
+        {
+            Console.WriteLine("Usage: CheckSummarizer <file> <-p>");
+            return;
+        }
+
+        var path = args[0];
+        var parallel = args.Length > 1;
         var checkSummarizer = new CheckSummarizer();
+        var result = string.Empty;
+        if (parallel)
+        {
+            if (args[1] == "-p")
+            {
+                result = Program.ToHexString(await checkSummarizer.SummarizeSystemObjectParallel(path));
+            }
+            else
+            {
+                result = "Usage: CheckSummarizer <file> <-p>";
+            }
+        }
+        else
+        {
+            result = Program.ToHexString(await checkSummarizer.SummarizeSystemObject(path));
+        }
+
+        Console.WriteLine(result);
+    }
+
+    private static string ToHexString(byte[] bytes)
+    {
+        var sb = new System.Text.StringBuilder(bytes.Length * 2);
+        foreach (byte b in bytes)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        return sb.ToString();
     }
 }
